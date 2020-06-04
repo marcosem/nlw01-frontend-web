effd1cd7a7b53be6fe20dc8d0b48f95b99f2f7dd
Marcos E Mathias <marcos-github@memathias.com>
2020-06-04 20:58:47 -0300

    Added State to main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
     <div>
-      <Header title="Hello World"/>
-      <h1>Conteúdo da Aplicação</h1>
+      <Header title={`Contador: ${counter}`}/>
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}></button>
     </div>
   );
 }
