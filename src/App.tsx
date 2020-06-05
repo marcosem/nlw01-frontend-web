2cb872b264684307f51c5867b8dacff85fb96407
Marcos E Mathias <marcos-github@memathias.com>
2020-06-04 21:25:18 -0300

    Styled Main Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Header from './Header';
+
+import Home from './pages/Home'
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title={`Contador: ${counter}`}/>
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}></button>
-    </div>
+    <Home />
   );
 }
 
